2d3c4b908eff06ff7c0ab118c04f8f1e2c846001
UnloadingGnat <jaavinjm123@gmail.com>
2022-03-19 02:00:02 -0400

    Token Fixes

--- gShell.py
+++ gShell.py
@@ -1,13 +1,11 @@
 from GnatScript.gs import run
 from colorama import Fore
 
-
 ##
 # gShell
 # @author UnloadingGnat
 # @date 2022-03-18
 # v 0.1.1
-
 
 print(Fore.GREEN + "GnatScript 0.1.0\n")
 print(Fore.RESET)
